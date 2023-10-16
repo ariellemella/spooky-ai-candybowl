0a1c84e4a4b9577e725f47012d2a06513b891963
Arielle <arielle@viam.com>
2023-10-16 12:14:05 -0400

    detections + jump scare functionality
    simple functionality

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import asyncio
-import os 
+import os
 
 from viam.robot.client import RobotClient
 from viam.rpc.dial import Credentials, DialOptions
 from viam.components.board import Board
 from viam.components.motor import Motor
 from viam.components.camera import Camera
+from viam.services.vision import VisionClient
 
 
 #Env Variables for Robot Connectivity 
 robot_secret = os.getenv('ROBOT_SECRET') 
 robot_address = os.getenv('ROBOT_ADDRESS') 
 
-
+#Robot Connectivity 
 async def connect():
     creds = Credentials(
         type='robot-location-secret',
@@ -23,26 +24,42 @@ async def connect():
     )
     return await RobotClient.at_address(robot_address, opts)
 
-async def handMovement(dcmotor):
-
+async def jumpscare(dcmotor):    
     #Hand jumps out, motor goes forward
-    await dcmotor.set_power(power=1)
-    await dcmotor.go_for(rpm=60, revolutions=1)
+    await dcmotor.set_power(power=.75)
+    await dcmotor.go_for(rpm=70, revolutions=1)
 
     #Hand goes in, motor goes backwards 
-    await dcmotor.set_power(power=-1)
-    await dcmotor.go_for(rpm=-60, revolutions=1)
+    await dcmotor.set_power(power=-.75)
+    await dcmotor.go_for(rpm=-70, revolutions=1)
+
+
+async def trickOrTreater(dcmotor, detections_test):
+    #Searching for trick or treaters to scare! 
+    while True: 
+        detections = await detections_test.get_detections_from_camera("pumpcam")
+        seen = False 
+
+        for d in detections:
+            if d.confidence > 0.6:
+                if d.class_name == "Person":
+                    print("gotcha!")
+                    seen = True
+        if seen: 
+            await jumpscare(dcmotor)
+        else: 
+            print ("no one is around to take some candy")
 
 
 async def main():
     robot = await connect()
     
-    #Components 
-    pi = Board.from_robot(robot, "pi")
+    #Components and Services 
     dcmotor = Motor.from_robot(robot, "dcmotor")
-    pumpcam = Camera.from_robot(robot, "pumpcam")
-    
-    await handMovement(dcmotor)
+    detections_test = VisionClient.from_robot(robot, "detections_test")
+
+    await trickOrTreater(dcmotor, detections_test)
+
     await robot.close()
 
 if __name__ == '__main__':
